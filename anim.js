f2ba724c1efdf68853dbf718dc86c57872ab39c2
Berrachedi-yousra <58655560+Berrachedi-yousra@users.noreply.github.com>
2021-06-11 17:21:53 +0100

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,19 @@
+function gohome(){
+    onclick=window.location.href='index.html';
+}
+
+function goeducation(){
+    onclick=window.location.href='education.html';
+}
+
+function goexperience(){
+    onclick=window.location.href='experience.html';
+}
+
+function goskills(){
+    onclick=window.location.href='skills.html';
+}
+
+function gocontact(){
+    onclick=window.location.href='contact.html';
+}
